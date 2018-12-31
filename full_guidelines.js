4ca1676147700225ac1821e78a9fe9539f7c2124
JoshLondin <46266102+JoshLondin@users.noreply.github.com>
2018-12-30 19:43:09 -0500

    Add files via upload

---
+++ full_guidelines.js
@@ -0,0 +1,51 @@
+const full_guidelines = {
+  "title": "SAFC Guidelines",
+  "type": "numeric",
+  "list": [
+    {
+      "title": "Definitions",
+      "list":  [section1]
+    }
+    //,
+    // {
+    //   "title": "Applicants",
+    //   "list":   section2
+    // },
+    // {
+    //   "title": "Request for Funds",
+    //   "list":  section3
+    // },
+    // {
+    //   "title": "Allocation",
+    //   "list":  section4
+    // },
+    // {
+    //   "title": "Budget Hearings",
+    //   "list":  section5
+    // },
+    // {
+    //   "title": "Appeals",
+    //   "list":  section6
+    // },
+    // {
+    //   "title": "Reversion of Funds",
+    //   "list":  section7
+    // },
+    // {
+    //   "title": "Categories of Supported Expenses",
+    //   "list":  section8
+    // },
+    // {
+    //   "title": "Performance Reporting",
+    //   "list":  section9
+    // },
+    // {
+    //   "title": "Calendar",
+    //   "list":  section10
+    // },
+    // {
+    //   "title": "Appendix A. Negotiated Rates",
+    //   "list":  appendix
+    // }
+  ]
+}
