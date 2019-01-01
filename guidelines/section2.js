3105c3fadeb2c2e8dc59165c6f60714812a04cfa
Josh Londin <jgl92@cornell.edu>
2019-01-01 18:21:04 -0500

    debug

--- guidelines/section2.js
+++ guidelines/section2.js
@@ -8,17 +8,17 @@ const section2= {
         "title": "To be eligible to request funds or to spend allocated funds an applicant must:",
         "type": "bullet"
         "list": [
-          {"title": 'register with and be approved by the Campus Activities Office by when the budgets are due in each semester'},
-          {"title": 'declare during the Campus Activities Office registration process that the group intends to apply for SAFC funding during that academic year; prior to the first deadline for funding requests in the fall semester, if funding is sought in the fall, prior to the first deadline for funding requests in the spring semester, if funding is sought in the spring;'},
-          {"title": 'have undergraduate members who comprise at least sixty percent of its total membership, excluding alumni members; and,'},
-          {"title": 'be in good standing with the Commission.'},
-          {"title": 'Officers of the applicant must also meet the following requirements:'},
-          {"title": 'president and treasurer must agree to abide by standards for ethical conduct; and,'},
-          {"title": 'president, treasurer, and advisor must affirm their approval for each request either by digital signature as the Commission provides on the specific budget.',
-          "list":["title":"Registered president, treasurer, and advisor must be three distinct individuals"]},
-          {"title": 'The signatures of the president, treasurer, and advisor must match the names listed on the group\'s profile page in Orgsync.'},
-          {"title": 'be in good standing with the Commission.'},
-          {"title": 'The organization must meet all eligibility requirements as well as submit both requests and supporting materials before the deadlines set by the Commission in order for the Commission to accept and review such requests and materials. The commission may not accept submissions from organizations that have not met the eligibility requirements.'},
+          'register with and be approved by the Campus Activities Office by when the budgets are due in each semester',
+          'declare during the Campus Activities Office registration process that the group intends to apply for SAFC funding during that academic year; prior to the first deadline for funding requests in the fall semester, if funding is sought in the fall, prior to the first deadline for funding requests in the spring semester, if funding is sought in the spring;',
+          'have undergraduate members who comprise at least sixty percent of its total membership, excluding alumni members; and,',
+          'be in good standing with the Commission.',
+           'Officers of the applicant must also meet the following requirements:',
+          'president and treasurer must agree to abide by standards for ethical conduct; and,',
+          {"title":'president, treasurer, and advisor must affirm their approval for each request either by digital signature as the Commission provides on the specific budget.',
+          "list":["Registered president, treasurer, and advisor must be three distinct individuals"],}
+          'The signatures of the president, treasurer, and advisor must match the names listed on the group\'s profile page in Orgsync.',
+          'be in good standing with the Commission.',
+          'The organization must meet all eligibility requirements as well as submit both requests and supporting materials before the deadlines set by the Commission in order for the Commission to accept and review such requests and materials. The commission may not accept submissions from organizations that have not met the eligibility requirements.'
         ]
       ]
     },
@@ -29,19 +29,19 @@ const section2= {
         {"title": "An organization which otherwise meets all basic requirements to receive funding will be deemed ineligible to receive funding if either of the following conditions are satisfied:",
         "type": "bullet"
         "list": [
-          {"title": 'The organization both shares an essentially identical purpose/mission with another'},
-          {"title": 'The organization functions in conjunction with one or more SAFC-funded organizations in such a way that both organizations are reasonably perceived as existing for the purpose of increasing the total amount of SAFC-funding received.'},
+        'The organization both shares an essentially identical purpose/mission with another',
+         'The organization functions in conjunction with one or more SAFC-funded organizations in such a way that both organizations are reasonably perceived as existing for the purpose of increasing the total amount of SAFC-funding received.'
         ]
       },
-        {"title":"Outside of the semesterly allocation decisions, the SAFC, by a majority vote of the Executive Board, will revoke an organization’s funding if it is deemed to be in violation of above requirements. The organization(s) in question may appeal this determination to the SAFC Appeals Board for review."}
+      "Outside of the semesterly allocation decisions, the SAFC, by a majority vote of the Executive Board, will revoke an organization’s funding if it is deemed to be in violation of above requirements. The organization(s) in question may appeal this determination to the SAFC Appeals Board for review."
       ]
     },
     {
       "title": 'Exception for Unforeseen Circumstances',
       "type": "blank"
       "list": [
-        {"title": "All organizations that are, at the time of their application, assigned Tier 4, Tier 5, Tier 6, or New Group (Tier 7) must demonstrate that the Treasurer, President, or Advisor attend at least one of three help sessions at a pre-announced date. If the Treasurer or President is unable to attend any of the three help sessions, the President or Treasurer may designate another member of the organization to attend a help session on their behalf."},
-        {"title": "Organizations that do not meet the above requirement forfeit their right to demand a SAFC Appeals Board Hearing, but may be granted such a hearing at the discretion of the the Chair of the SAFC Appeals Board, for truly exceptional circumstances. The organization will still retain the right to a budget hearing."}
+        "All organizations that are, at the time of their application, assigned Tier 4, Tier 5, Tier 6, or New Group (Tier 7) must demonstrate that the Treasurer, President, or Advisor attend at least one of three help sessions at a pre-announced date. If the Treasurer or President is unable to attend any of the three help sessions, the President or Treasurer may designate another member of the organization to attend a help session on their behalf.",
+        "Organizations that do not meet the above requirement forfeit their right to demand a SAFC Appeals Board Hearing, but may be granted such a hearing at the discretion of the the Chair of the SAFC Appeals Board, for truly exceptional circumstances. The organization will still retain the right to a budget hearing."
       ]
     },
     {
@@ -56,33 +56,31 @@ const section2= {
              "title":"Notify both the SAFC Co-Chairs via email to their netid’s available on the Commission website to request a 24 hour extension from the original funding request deadline:",
              "type":"bullet",
              "list":[
-               {"title":"This request must include the evidence required below; and"},
-               {"title":"Must be submitted within 24 hours after the funding request deadline"}
+              "This request must include the evidence required below; and",
+              "Must be submitted within 24 hours after the funding request deadline"
              ]
            },
            {
              "title":"Complete the remainder of their application before the eventual extended deadline. Eligible unforeseen circumstances and associated evidence are the following:",
              "list":[
-               {"title":"Negligence of advisor; Required evidence: screenshot of written communication with advisor three days prior to the funding request deadline in regards to funding request approval."},
-               {"title":"Injury or illness of treasurer, president, or advisor; Required evidence: note from a physician or medical professional with date of injury and explanation of event."}
-               {"title":"Death of treasurer, president, or advisor. Required evidence: at the discretion of the SAFC co-chairs."}
+              "Negligence of advisor; Required evidence: screenshot of written communication with advisor three days prior to the funding request deadline in regards to funding request approval.",
+              "Injury or illness of treasurer, president, or advisor; Required evidence: note from a physician or medical professional with date of injury and explanation of event.",
+               "Death of treasurer, president, or advisor. Required evidence: at the discretion of the SAFC co-chairs."
              ]
            },
          ]
       },
+      "The SAFC co-chairs will evaluate the evidence provided and whether or not the circumstance is unforeseen, and notify the organization as decision before funding request reviews commence.",
       {
-       "title": "The SAFC co-chairs will evaluate the evidence provided and whether or not the circumstance is unforeseen, and notify the organization as decision before funding request reviews commence."
-       },
-       {
         "title": "Requests for extension will be evaluated based on:",
         "type": "numeric",
         "list":[
-          {"title":"Demonstration of foresight regarding SAFC rules and deadlines;"},
-          {"title":"Severity of the unforeseen circumstance;"},
-          {"title":"Extent of ineligibility (i.e. completeness of application)"}
+          "Demonstration of foresight regarding SAFC rules and deadlines;",
+          "Severity of the unforeseen circumstance;",
+          "Extent of ineligibility (i.e. completeness of application)"
         ]
-        },
-        {"title":"The submission of an extension request should not be construed as a granting of said request. The decision to accept or deny a request for an extension is not subject to appeal. Again, the commission may not accept funding request submissions from organizations that have not met the eligibility requirements."}
+      },
+        "The submission of an extension request should not be construed as a granting of said request. The decision to accept or deny a request for an extension is not subject to appeal. Again, the commission may not accept funding request submissions from organizations that have not met the eligibility requirements."
        ]
     },
   ]
@@ -91,7 +89,7 @@ const section2= {
   "title":"Ethical Conduct",
   "type": "blank",
   "list":[
-    {"title":"Officers of applicants must agree to the Statement on Ethical Conduct prepared by Campus Activities Office by digital or written signature as they provides."}
+    "Officers of applicants must agree to the Statement on Ethical Conduct prepared by Campus Activities Office by digital or written signature as they provides."
   ]
 },
 {
@@ -104,8 +102,8 @@ const section2= {
         "title":"To prevent misuse of commission funds, the Executive Board of the Commission may act to temporarily suspend or permanently revoke allocated funds. The Board may revoke funds of an organization if it finds that the organization:",
         "type":"bullet",
         "list":[
-          {"title":"does not meet one or more eligibility requirements; or,"},
-          {"title":"acted in violation of the Statement on Ethical Conduct."},
+          "does not meet one or more eligibility requirements; or,",
+          "acted in violation of the Statement on Ethical Conduct."
         ]
       ]
       },
@@ -113,13 +111,14 @@ const section2= {
         "title":"Suspension of Funding",
         "type": "blank",
         "list":[
-          "title":"Either the Office of the Dean of Students or the Chair(s) of the Commission may temporarily suspend an organization’s funds at any time. Any action to temporarily suspend funding of an applicant organization shall last no more than ten business days. Such a decision takes effect only after the following are informed:",
+          {"title":"Either the Office of the Dean of Students or the Chair(s) of the Commission may temporarily suspend an organization’s funds at any time. Any action to temporarily suspend funding of an applicant organization shall last no more than ten business days. Such a decision takes effect only after the following are informed:",
           "type":"numeric",
           "list":[
-            {"title":"the Office of the Dean of Students;"},
-            {"title":"the Chair(s); and,"},
-            {"title":"the president, treasurer, and advisor of the affected organization."},
+            "the Office of the Dean of Students;",
+            "the Chair(s); and,",
+            "the president, treasurer, and advisor of the affected organization."
           ]
+        }
           ]
         },
         {
@@ -130,24 +129,24 @@ const section2= {
                "title":"To revoke funding of an organization, the Commission must follow this procedure:",
                "type":"numeric",
                "list":[
-                 {"title":"the Chair(s) of the Commission must contact the officers and advisor of the organization, explaining the Commission’s intent and reasoning for proposing revocation of funding; and,"},
+                 "the Chair(s) of the Commission must contact the officers and advisor of the organization, explaining the Commission’s intent and reasoning for proposing revocation of funding; and,",
                  {"title":"the Chair(s) of the Commission, or a designee, must arrange for a meeting between the Executive Committee and the organization so the organization may:",
                  "list":[
-                   {"title":"hear the Commission’s intent and reasoning,"},
-                   {"title":"examine any evidence associated with the proposed revocation of funding,"},
-                   {"title":"present its case and additional evidence to the Commission, and"},
-                   {"title":"negotiate a mutually acceptable remedy; and,"}
+                   "hear the Commission’s intent and reasoning,",
+                   "examine any evidence associated with the proposed revocation of funding,",
+                   "present its case and additional evidence to the Commission, and",
+                   "negotiate a mutually acceptable remedy; and,"
                  ]
                },
-               {"title":"the Executive Board, after deliberating in closed session, may vote to revoke funding."}
+               "the Executive Board, after deliberating in closed session, may vote to revoke funding."
                ]
            },
-           {"title":"The decision to revoke funding is subject to appeal by the Appeals Board. Any funds that are revoked remain unavailable to the organization until the appeal process is resolved."}
+           "The decision to revoke funding is subject to appeal by the Appeals Board. Any funds that are revoked remain unavailable to the organization until the appeal process is resolved."
         ]
       },
       {
         "title":"Limitations",
         "type":"blank",
-        "list" :[{"title":"The Executive Board may not revoke funding for more than one academic year and may reverse a decision to revoke funds at any time."}]
+        "list" :["The Executive Board may not revoke funding for more than one academic year and may reverse a decision to revoke funds at any time."]
       }
 }
